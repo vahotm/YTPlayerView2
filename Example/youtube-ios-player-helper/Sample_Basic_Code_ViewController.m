2c705c40340b379ff26ca5046f97e5b29db9b02d
Ono Masashi <masashi.ono@linecorp.com>
2016-03-23 18:48:32 +0900

    Fixed example project (now works!!)
    At last it works as intended. I'm going to add more testbed examples to try out options.

--- Example/youtube-ios-player-helper/Sample_Basic_Code_ViewController.m
+++ Example/youtube-ios-player-helper/Sample_Basic_Code_ViewController.m
@@ -18,18 +18,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    // ALWAYS ALWAYS make sure that the `UIViewController.automaticallyAdjustsScrollViewInsets` is set to NO!
+    // This will automatically adds up unwanted content inset to the `YTPlayerView.webView.scrollView`, breaking viewports of the WKWebView completely when rotating the device (or possibly other re-layouting triggers).
+    // Also it seems like this *unwanted content inset* issue only happens when we use `UIViewController.topLayoutGuide` to auto-layout the view, possibly only on iOS 8.
+    // Anyway, remember to check the `YTPlayerView.webView.scrollView.contentInset` asap when you find the video size looks broken and that could be caused by `UIViewController.automaticallyAdjustsScrollViewInsets`.
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    
     self.title = @"Instantiate using Code";
     self.view.backgroundColor = [UIColor blackColor];
     
     self.playerView = [[YTPlayerView alloc] initWithFrame:CGRectZero];
     self.playerView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.playerView.backgroundColor = [UIColor redColor];
     [self.view addSubview:self.playerView];
     
-    NSDictionary *views = @{@"playerView": self.playerView};
-    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[playerView]|" options:0 metrics:nil views:views]];
-    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[playerView]" options:0 metrics:nil views:views]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.view attribute:NSLayoutAttributeLeadingMargin relatedBy:NSLayoutRelationEqual toItem:self.playerView attribute:NSLayoutAttributeLeading multiplier:1.0 constant:0]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.view attribute:NSLayoutAttributeTrailingMargin relatedBy:NSLayoutRelationEqual toItem:self.playerView attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:0]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[topGuide]-20-[playerView]" options:0 metrics:nil views:@{@"topGuide": self.topLayoutGuide, @"playerView": self.playerView}]];
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.view attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.playerView attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0]];
     [self.playerView addConstraint:[NSLayoutConstraint constraintWithItem:self.playerView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.playerView attribute:NSLayoutAttributeHeight multiplier:(16.0/9.0) constant:0]];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self.playerView loadPlayerWithVideoId:@"M7lc1UVf-VE"];
 }
 
 @end
